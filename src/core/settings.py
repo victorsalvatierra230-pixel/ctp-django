257c33cea893cc8923da2dda5b84135550cc7a30
salvatierra1 <victordanielsalvatierra6@gmail.com>
2025-08-05 21:48:08 -0300

    correction files

--- src/core/settings.py
+++ src/core/settings.py
@@ -128,9 +128,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR, 'static'
-    ]
-
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Login
