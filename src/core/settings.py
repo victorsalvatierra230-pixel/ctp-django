35c069418b2ff8c5d2079e8252beafaa326d707d
salvatierra1 <victordanielsalvatierra6@gmail.com>
2025-08-05 21:58:27 -0300

    correction files

--- src/core/settings.py
+++ src/core/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, "templates"], 
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # solo si tenés esa carpeta
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'src', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Producción: servir archivos estáticos con WhiteNoise
